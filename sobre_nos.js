22ce2e18ce57efab8aec4269a142c7902c992e45
ViniciusLimaSouza <vini.10.lima.2005@gmail,com>
2024-11-18 19:32:50 -0300

    Mudanças

--- sobre_nos.js
+++ sobre_nos.js
@@ -3,15 +3,15 @@ const buttonAccount = document.querySelector('#button-create-acount')
 const buttonCart = document.querySelector('#button-shopping-cart')
 
 function buttonLoginClicked() {
-    window.location.href = "../login.html";
+    window.location.href = "login.html";
     console.log("clicked");
   }
 
 function buttonCreatAccountClicked() {
-    window.location.href = "../cadastro.html";
+    window.location.href = "cadastro.html";
     console.log("criar conta");
   }
 
 function buttonShoppingCart() {
-    window.location.href = "../carrinho.html";
+    window.location.href = "carrinho.html";
   }
